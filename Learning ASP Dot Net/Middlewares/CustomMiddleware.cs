67b74ae7696d96405c60310bf7480e7dbf1f1bc9
yogeshdvish <vishwakarmayogesh2407@gmail.com>
2026-02-17 09:35:00 +0530

    middle ware practice

---
+++ Learning ASP Dot Net/Middlewares/CustomMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace Learning_ASP_Dot_Net.Middlewares
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync (HttpContext context)
+        {
+             Console.WriteLine("Custom Middleware Executed");
+
+            await _next(context);
+            
+            Console.WriteLine("Custom Middleware Executed After Request");
+        }
+    }
+}
